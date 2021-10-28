41f2bcaca89583fa930d998609fd31a8e31281ed
Shalom David Effiom <shalom@gravitasfinancellc.com>
2021-10-28 16:29:27 +0100

    Demo for CSS Modules on About Page

--- pages/about.js
+++ pages/about.js
@@ -9,10 +9,11 @@ export default function About() {
 				<Container>
 					<Row>
 						<Col md={5}>
-							image goes here
+							<img src="/img/logo.png" className={Styles.Logo} alt="About Kenaya" />
 						</Col>
 						<Col md={7}>
-							text goes here
+							<p className={Styles.AboutText}>text goes here</p>
+							<p className={Styles.AboutTextTwo}>text goes here</p>
 						</Col>
 					</Row>
 				</Container>
