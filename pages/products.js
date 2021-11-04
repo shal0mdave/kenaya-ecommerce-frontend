04bf3d4ece9b555613a93e43e4e5c0fb0bc541aa
Kashala10 <ilonakashala7@gmail.com>
2021-11-04 23:05:48 +0400

    data update

--- pages/products.js
+++ pages/products.js
@@ -1,10 +1,11 @@
 const products = [
     {
-      name: "Camel Pants",
-      imageUrl:"",
+      name: "Off Shoulder",
+      collection:"Oasis",
+      imageUrl:"https://res.cloudinary.com/kenaya/image/upload/v1636029583/IMG_0032_t6hlve.jpg",
       id: 1,
       description:
-        "An effortless & elegant high waisted pant with an elastic waistband.",
+        "An effortless & elegant off shoulder top with an elastic band.",
       composition:"Peach skin",
       wearing:"The model is 1.72 m wearing size S/36 (FR)",
       price: 1599.00,
@@ -14,7 +15,8 @@ const products = [
 
     {
       name: "Ataga Pants ",
-      imageUrl:"",
+      collection:"Oasis",
+      imageUrl:"https://res.cloudinary.com/kenaya/image/upload/v1636029521/IMG_0013_uf4jmc.jpg",
       id: 2,
       description: "Flowy Pants with a high waist and lining shorts underneath. It has an adjustable waist drawstring and a long slit on the side.",
       composition:"Peach skin",
@@ -25,7 +27,8 @@ const products = [
     },
     {
       name: "Dahlia Shirt",
-      imageUrl:"",
+      collection:"Blossom",
+      imageUrl:"https://res.cloudinary.com/kenaya/image/upload/v1636029513/IMG_0074_w3dmn8.jpg",
       id: 3,
       description:
         "Bring in more colours into your wardrobe with this floral shirt that has the option to be tied in the front.",
@@ -37,7 +40,8 @@ const products = [
     },
     {
       name: "Dahlia Maxi skirt",
-      imageUrl:"",
+      collection:"Blossom",
+      imageUrl:"https://res.cloudinary.com/kenaya/image/upload/v1636029509/IMG_0005_nhjxll.jpg",
       id: 4,
       description:
         "Flatter yourself in this Floral print maxi wrap skirt. ",
@@ -49,7 +53,8 @@ const products = [
     },
     {
       name: "Jasmine Playsuit",
-      imageUrl:"",
+      collection:"Blossom",
+      imageUrl:"https://res.cloudinary.com/kenaya/image/upload/v1636029595/IMG_0042_smyjtl.jpg",
       id: 5,
       description:
         "An effortless & elegant high waisted pant with an elastic waistband.",
@@ -62,7 +67,21 @@ const products = [
     },
     {
       name: "Jasmine High-Waist Shorts",
-      imageUrl:"",
+      collection:"Blossom",
+      imageUrl:"https://res.cloudinary.com/kenaya/image/upload/v1636029529/IMG_0094_b97fzs.jpg",
+      id: 6,
+      description:
+        "High Waisted shorts with deep pockets. ",
+      composition:"Peach skin",
+      wearing:"The model is 1.75m wearing size L (FR)",
+      price: 1150.00,
+      currencyFormat: "Rs",
+      availableSizes: [["S"],["M"] ,["L"] ,["XL"] ]  ,
+    },
+    {
+      name: "Zinnia Dress",
+      collection:"Blossom",
+      imageUrl:"https://res.cloudinary.com/kenaya/image/upload/v1636029529/IMG_0094_b97fzs.jpg",
       id: 6,
       description:
         "High Waisted shorts with deep pockets. ",
